ae72d2bb1b57b9f0ce86d85746f9d8edae1964f9
Joshua Davari <joshua.davari@gmail.com>
2024-05-24 17:33:57 -0700

    whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css'
 import EmployeeBenefits from './components/employee-benefits';
 
 const App = () => <>
-  <div className='app-container'>
-    <h2>Benefits Cost Calculator</h2>
-    <EmployeeBenefits />
-  </div>
+    <div className='app-container'>
+        <h2>Benefits Cost Calculator</h2>
+        <EmployeeBenefits />
+    </div>
 </>;
 
 export default App;
